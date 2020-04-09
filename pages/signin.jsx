d33aceecba3e6513f9c558487dd1d2433057e154
sidd82 <psiddhesh14@gmail.com>
2020-04-10 03:08:02 +0530

    Sign In Added

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import apiUrl from "../config/apiUrl";
 
 const Signin = () => {
+  const [data, setData] = useState({
+    email: "psiddhesh14@gmail.com",
+    password: "Si78757875",
+    fingureId: "123",
+  });
+
+  const handleSignUpSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const signUpRes = await Axios.post(
+        `${apiUrl}/api/auth/user/sign-in`,
+        data,
+        {
+          withCredentials: true,
+          headers: {
+            "x-api-key": process.env.X_API_KEY,
+          },
+        }
+      );
+      console.log(signUpRes);
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  };
   return (
     <div>
-      <h1>Sign In</h1>
+      <form onSubmit={handleSignUpSubmit}>
+        {/* Input For Email */}
+        <div>
+          <label htmlFor="email" style={{ display: "block" }}>
+            Email
+          </label>
+          <input
+            type="email"
+            name="name"
+            placeholder="Enter Email Id"
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+          />
+        </div>
+
+        {/* Input For Password */}
+        <div>
+          <label htmlFor="password" style={{ display: "block" }}>
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter pasword"
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </div>
   );
 };
