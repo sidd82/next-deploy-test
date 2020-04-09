d2571da10b432f55d70c443554a4c92f945e7d55
sidd82 <psiddhesh14@gmail.com>
2020-04-09 23:15:04 +0530

    Removed useEffet From _app For Testing

--- pages/_app.js
+++ pages/_app.js
@@ -9,19 +9,19 @@ function MyApp({ Component, pageProps }) {
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
-  useEffect(() => {
-    console.log("Ram");
-    (async function () {
-      setIsRender(true);
-      const response = await Axios.get("http://localhost:5000/api/auth/user", {
-        withCredentials: true,
-        headers: {
-          api_auth: "Si78757875",
-        },
-      });
-      setIsRender(false);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   console.log("Ram");
+  //   (async function () {
+  //     setIsRender(true);
+  //     const response = await Axios.get("http://localhost:5000/api/auth/user", {
+  //       withCredentials: true,
+  //       headers: {
+  //         api_auth: "Si78757875",
+  //       },
+  //     });
+  //     setIsRender(false);
+  //   })();
+  // }, []);
   return isRender ? (
     <>
       <p>Loading...</p>
